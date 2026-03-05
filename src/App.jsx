c1c481ea100320996d9c8bbd1942932a533297d7
Swarnadeep  Dey <swarnadeep2016dey@gmail.com>
2026-03-05 14:08:21 +0530

    Changed use state to 0

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  let [counter, setCounter] = useState(5);
+  let [counter, setCounter] = useState(0);
 
   const addValue = () => {
     if (counter < 19) {
